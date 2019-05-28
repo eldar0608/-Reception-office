ba601ede271ba6836f2d2f98641ca82e27c3377a
eldar <eldasharplion@gmail.com>
2019-05-28 15:46:08 +0600

    Create and init project

---
+++ src/main/java/kg/it/receptionoffice/ReceptionOfficeApplication.java
@@ -0,0 +1,13 @@
+package kg.it.receptionoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceptionOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceptionOfficeApplication.class, args);
+	}
+
+}
